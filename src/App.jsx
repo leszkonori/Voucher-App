154137bee85e855f4926cbcf35986fea314c9cb5
leszkonori <leszko.n3@gmail.com>
2024-12-10 23:09:21 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import VoucherManagement from './VoucherManagement';
+
+function App() {
+  return (
+    <div className="App">
+      <VoucherManagement/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/VoucherManagement.jsx
@@ -0,0 +1,6 @@
+export default function VoucherManagement() {
+
+    return (
+        <p>Hi! This is the Voucher Management interface</p>
+    );
+}
